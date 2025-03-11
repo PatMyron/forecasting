295096c8c280d39910b72a2ddfa927fcedeea3a1
Pat Myron <pmyron@nd.edu>
2025-03-10 23:25:24 -0400

    polymarket https://docs.polymarket.com/ https://github.com/PatMyron/forecasting/issues/5

---
+++ polymarket/markets.py
@@ -0,0 +1,25 @@
+import json
+import requests
+import requests_cache
+from datetime import datetime, timedelta, UTC
+requests_cache.install_cache('cache')
+offset = 0
+markets, unclosed, soon = [], [], []
+while requests.get('https://gamma-api.polymarket.com/markets?limit=500&offset=' + str(offset)).json():
+  for market in requests.get('https://gamma-api.polymarket.com/markets?limit=500&offset=' + str(offset)).json():
+    markets.append(market)
+    if not market['closed']:
+      unclosed.append(market)
+      try:
+        if datetime.fromisoformat(market['endDate']) < datetime.now(UTC) + timedelta(days=60):
+          soon.append(market)
+      except:
+        if datetime.fromisoformat(market['events'][0]['endDate']) < datetime.now(UTC) + timedelta(days=60):
+          soon.append(market)
+  offset += 500
+with open('all.json', 'w') as f:
+  json.dump(markets, f, indent=0)
+with open('open.json', 'w') as f:
+  json.dump(unclosed, f, indent=0)
+with open('soon.json', 'w') as f:
+  json.dump(soon, f, indent=0)
