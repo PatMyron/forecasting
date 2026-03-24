e0702dd1756345dc42c907ff3ba6bd1c97b5a637
Pat Myron <pmyron@nd.edu>
2026-03-23 20:36:18 -0400

    Kalshi: requests/urllib3 retry 429s w/ backoff https://docs.python-requests.org/en/latest/user/advanced/#example-automatic-retries https://docs.python-requests.org/en/latest/api/#requests.adapters.HTTPAdapter https://urllib3.readthedocs.io/en/stable/reference/urllib3.util.html https://stackoverflow.com/q/23267409/retry-python-requests

--- kalshi/events.py
+++ kalshi/events.py
@@ -1,6 +1,14 @@
 import json
 import requests
+from urllib3 import Retry
 session = requests.Session()
+session.mount('https://', requests.adapters.HTTPAdapter(max_retries=Retry(
+  total=12,
+  status_forcelist=[429],
+  backoff_factor=0.1,
+  backoff_jitter=0.2,
+  backoff_max=2,
+)))
 url = 'https://api.elections.kalshi.com/trade-api/v2/events?limit=200&status=open&cursor='
 response = session.get(url).json()
 unclosed = []
--- kalshi/markets.py
+++ kalshi/markets.py
@@ -1,7 +1,15 @@
 import json
 import requests
 from datetime import datetime, timedelta, UTC
+from urllib3 import Retry
 session = requests.Session()
+session.mount('https://', requests.adapters.HTTPAdapter(max_retries=Retry(
+  total=12,
+  status_forcelist=[429],
+  backoff_factor=0.1,
+  backoff_jitter=0.2,
+  backoff_max=2,
+)))
 url = 'https://api.elections.kalshi.com/trade-api/v2/markets?limit=1000&status=open'
 response = session.get(url).json()
 markets, unclosed, soon, sooner, soonest = [], [], [], [], []
