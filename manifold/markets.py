b548d7bb60c2bc467d5d2086e0292edc8366de6a
Pat Myron <pmyron@nd.edu>
2026-03-24 12:39:45 -0400

    Manifold: requests/urllib3 retry 429s w/ backoff https://docs.python-requests.org/en/latest/user/advanced/#example-automatic-retries https://docs.python-requests.org/en/latest/api/#requests.adapters.HTTPAdapter https://urllib3.readthedocs.io/en/stable/reference/urllib3.util.html https://stackoverflow.com/q/23267409/retry-python-requests

--- manifold/markets.py
+++ manifold/markets.py
@@ -1,7 +1,15 @@
 import json
 import requests
 import time
+from urllib3 import Retry
 session = requests.Session()
+session.mount('https://', requests.adapters.HTTPAdapter(max_retries=Retry(
+  total=12,
+  status_forcelist=[429],
+  backoff_factor=0.1,
+  backoff_jitter=0.2,
+  backoff_max=2,
+)))
 url = 'https://api.manifold.markets/v0/markets?limit=1000'
 response = session.get(url).json()
 markets, unclosed, soon = [], [], []
--- manifold/users.py
+++ manifold/users.py
@@ -1,6 +1,14 @@
 import json
 import requests
+from urllib3 import Retry
 session = requests.Session()
+session.mount('https://', requests.adapters.HTTPAdapter(max_retries=Retry(
+  total=12,
+  status_forcelist=[429],
+  backoff_factor=0.1,
+  backoff_jitter=0.2,
+  backoff_max=2,
+)))
 url = 'https://api.manifold.markets/v0/users?limit=1000'
 response = session.get(url).json()
 users, whales, losers = [], [], []
